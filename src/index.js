f4e10807b4b1a8ba4d7576bed07a3e31c02a168b
Andres Ruiz <andrsruiz@gmail.com>
2020-06-06 15:37:24 -0500

    Add my function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log("Works");
+function component() {
+	const element = document.createElement('div');
+
+	// Using my function
+	element.innerHTML = 'Works';
+	return element;
+}
+
+document.body.appendChild(component());

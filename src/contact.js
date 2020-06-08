5665928825a1dc84d948483a03bb893202a2e013
Andres Ruiz <andrsruiz@gmail.com>
2020-06-08 17:48:48 -0500

    Add Form in Contact Page

--- src/contact.js
+++ src/contact.js
@@ -1,13 +1,49 @@
 const Contact = (() => {
-	const TEXT = "Diam volutpat commodo sed egestas egestas fringilla phasellus faucibus scelerisque eleifend donec pretium vulputate sapien nec sagittis aliquam malesuada bibendum. Cras adipiscing enim eu turpis egestas pretium aenean pharetra, magna!";
 	var contactDivDOM;
 
 	const render = (body) => {
 		body.clearMain();
 		contactDivDOM = document.createElement("div");
-		let h3 = document.createElement("h3");
-		h3.innerHTML = TEXT;
-		contactDivDOM.appendChild(h3);
+		contactDivDOM.id = "contactdiv";
+		contactDivDOM.className = "contentdiv";
+		let form = document.createElement("form");
+		form.id = "contactForm";
+		/* name input */
+		let name = document.createElement("input");
+		let nameLabel = document.createElement("label");
+		name.type = "text";
+		name.id = "name";
+		nameLabel.for = "name";
+		nameLabel.innerHTML = "Name";
+		nameLabel.style.textAlign = "left";
+		/* number input */
+		let number = document.createElement("input");
+		let numberLabel = document.createElement("label");
+		number.type = "number";
+		number.id = "number";
+		numberLabel.for = "number";
+		numberLabel.innerHTML = "Whatsapp number";
+		numberLabel.style.textAlign = "left";
+		/* message */
+		let message = document.createElement("textarea");
+		let messageLabel = document.createElement("label");
+		message.id = "message";
+		messageLabel.for = "message";
+		messageLabel.innerHTML = "What do you want to say to us?";
+		messageLabel.style.textAlign = "left";
+		/* submit */
+		let submit = document.createElement("input");
+		submit.type = "submit";
+		submit.id = "submit";
+		submit.value = "Send";
+		form.appendChild(nameLabel);
+		form.appendChild(name);
+		form.appendChild(numberLabel);
+		form.appendChild(number);
+		form.appendChild(messageLabel);
+		form.appendChild(message);
+		form.appendChild(submit);
+		contactDivDOM.appendChild(form);
 		body.getMain().appendChild(contactDivDOM);
 	}
 

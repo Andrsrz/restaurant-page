ab644c6195c45674ccd73785319ff0d1cb52d377
Andres Ruiz <andrsruiz@gmail.com>
2020-06-08 14:41:43 -0500

    Header is now a Class

--- src/header.js
+++ src/header.js
@@ -1,23 +1,28 @@
 import { getBody } from './domBody.js';
 
-const render = () => {
-	let headerDOM = document.createElement("header");
-	let navbarDOM = document.createElement("nav");
-	let buttonAboutUs = document.createElement("button");
-	buttonAboutUs.innerHTML = "About Us";
-	let buttonMenu = document.createElement("button");
-	buttonMenu.innerHTML = "Menu";
-	let buttonContact = document.createElement("button");
-	buttonContact.innerHTML = "Contact";
-	let buttonsDOM = [];
-	buttonsDOM.push(buttonAboutUs);
-	buttonsDOM.push(buttonMenu);
-	buttonsDOM.push(buttonContact);
-	for(let i = 0; i < buttonsDOM.length; i++){
-		navbarDOM.appendChild(buttonsDOM[i]);
+const Header = (() => {
+	const render = () => {
+		let headerDOM = document.createElement("header");
+		let navbarDOM = document.createElement("nav");
+		let buttonAboutUs = document.createElement("button");
+		buttonAboutUs.innerHTML = "About Us";
+		let buttonMenu = document.createElement("button");
+		buttonMenu.innerHTML = "Menu";
+		let buttonContact = document.createElement("button");
+		buttonContact.innerHTML = "Contact";
+		let buttonsDOM = [];
+		buttonsDOM.push(buttonAboutUs);
+		buttonsDOM.push(buttonMenu);
+		buttonsDOM.push(buttonContact);
+		for(let i = 0; i < buttonsDOM.length; i++){
+			buttonsDOM[i].setAttribute("class", "button");
+			navbarDOM.appendChild(buttonsDOM[i]);
+		}
+		headerDOM.appendChild(navbarDOM);
+		getBody().appendChild(headerDOM);
 	}
-	headerDOM.appendChild(navbarDOM);
-	getBody().appendChild(headerDOM);
-}
 
-export { render };
+	return { render };
+})();
+
+export { Header };

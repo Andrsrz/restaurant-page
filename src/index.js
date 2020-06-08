3cc70f60dad3c5f756641cc3a93535b3c540c212
Andres Ruiz <andrsruiz@gmail.com>
2020-06-08 16:22:10 -0500

    Add Tab Logic

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
-import { render } from './header';
+import { Body } from './domBody.js';
+import { Header } from './header.js';
+import { About } from './about.js';
 
-render();
+Header.render(Body.getBody());
+
+const Index = (() => {
+	const tabsLogic = () => {
+		let buttons = Header.getButtons();
+		for(let i = 0; i < buttons.length; i++){
+			buttons[i].addEventListener('clicl', (event) => {
+				moveToTab(i);
+			});
+		}
+	}
+
+	const moveToTab = (index) => {
+		switch(index){
+			case 0:
+				About.render(Body.getBody());
+				break;
+			case 1:
+				break;
+			case 2:
+				break;
+		}
+	}
+
+	return { tabsLogic };
+})();
+
+Index.tabsLogic();
